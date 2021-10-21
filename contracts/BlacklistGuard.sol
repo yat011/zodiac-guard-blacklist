a594777ebcadb3f7a854b3733485db5127eac27e
TY <devyat192002@gmail.com>
2021-10-22 01:14:04 +0800

    Change compiler version

--- contracts/BlacklistGuard.sol
+++ contracts/BlacklistGuard.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity 0.8.6;
+pragma solidity 0.8.9;
 
 import "@gnosis.pm/zodiac/contracts/guard/BaseGuard.sol";
 import "@gnosis.pm/zodiac/contracts/factory/FactoryFriendly.sol";
