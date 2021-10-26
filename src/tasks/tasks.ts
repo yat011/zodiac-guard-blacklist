9fac9863f30f1e0b1be55afa9ac3ae29b0c87571
TY <devyat192002@gmail.com>
2021-10-26 21:31:48 +0800

    Added example task for combining blacklistGuard and Delay

--- src/tasks/tasks.ts
+++ src/tasks/tasks.ts
@@ -5,7 +5,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { CONTRACT_ADDRESSES, CONTRACT_ABIS } from "@gnosis.pm/zodiac";
 import { getSafeSingletonDeployment } from "@gnosis.pm/safe-deployments";
 import { deployAndSetUpModule } from "./util";
-import { Contract, utils } from "ethers";
+import { Contract, Signer, utils } from "ethers";
 import { keccak256 } from "ethers/lib/utils";
 import { Guard } from "../../typechain";
 import assert from "assert";
@@ -176,16 +176,9 @@ task("linkAvatarAndGuard")
         const avatar = new Contract(taskArgs.avatar, safeDeployment.abi, hardhatRuntime.ethers.provider);
 
 
-        const data = avatar.interface.encodeFunctionData("setGuard(address)", [guard.address]);
-        const nonce = await avatar.nonce()
-        const addressZero = hardhatRuntime.ethers.constants.AddressZero
-
         const [user1] = await hardhatRuntime.ethers.getSigners();
-        const transHash = await avatar.connect(user1).getTransactionHash(avatar.address, 0, data, 0, 0, 0, 0, addressZero, addressZero, nonce);
-
-        const typedDataHash = utils.arrayify(transHash)
-        const signature = (await user1.signMessage(typedDataHash)).replace(/1b$/, "1f").replace(/1c$/, "20");
-        await avatar.connect(user1).execTransaction(avatar.address, 0, data, 0, 0, 0, 0, addressZero, addressZero, signature);
+        const data = avatar.interface.encodeFunctionData("setGuard(address)", [guard.address]);
+        await sendSafeTransaction(user1, avatar, avatar.address, data, hardhatRuntime);
 
 
     });
@@ -229,9 +222,6 @@ task("deployDelayProxy", "Deploys a Guard")
 
         const factorAddress = CONTRACT_ADDRESSES[chainId]['factory']
         const factorABI = CONTRACT_ABIS['factory'];
-
-
-
 
         const { transaction } = deployAndSetUpModule(
             CONTRACT_ADDRESSES[chainId]['delay'],
@@ -284,18 +274,183 @@ task("enableDelayModule")
         const safeDeployment = getSafeSingletonDeployment({ version: "1.3.0" });
         assert(safeDeployment !== undefined);
         const avatar = new Contract(taskArgs.avatar, safeDeployment.abi, hardhatRuntime.ethers.provider);
+
+
         await delayModifier.connect(user1).enableModule(avatar.address);
 
-
-
         const data = avatar.interface.encodeFunctionData("enableModule(address)", [delayModifier.address]);
-        const nonce = await avatar.nonce()
-        const addressZero = hardhatRuntime.ethers.constants.AddressZero
+        await sendSafeTransaction(user1, avatar, avatar.address, data, hardhatRuntime);
 
-        const transHash = await avatar.connect(user1).getTransactionHash(avatar.address, 0, data, 0, 0, 0, 0, addressZero, addressZero, nonce);
-        const typedDataHash = utils.arrayify(transHash)
-        const signature = (await user1.signMessage(typedDataHash)).replace(/1b$/, "1f").replace(/1c$/, "20");
-        await avatar.connect(user1).execTransaction(avatar.address, 0, data, 0, 0, 0, 0, addressZero, addressZero, signature);
     });
+
+
+task("sendSetTargetTxToDelay", "Block a target address.")
+    .addParam(
+        "avatar",
+        "The address of the guard that you are seting up.",
+        undefined,
+        types.string
+    )
+    .addParam("delay", "Address of the delay", undefined, types.string)
+    .addParam(
+        "guard",
+        "The address of the guard that you are seting up.",
+        undefined,
+        types.string
+    )
+    .addParam(
+        "target",
+        "The target address to be blocked.",
+        undefined,
+        types.string
+    )
+    .addParam(
+        "blockall",
+        "Block all access to the Target",
+        false,
+        types.boolean
+    )
+    .addParam(
+        "blockdelegate",
+        "Block delegatedCall",
+        false,
+        types.boolean
+    )
+    .addParam(
+        "funcsig",
+        "The signature of the target blocked function",
+        null,
+        types.string
+    )
+    .addParam(
+        "blockfunc",
+        "Block the target function",
+        false,
+        types.boolean
+    )
+    .setAction(async (taskArgs, hardhatRuntime) => {
+        const [caller] = await hardhatRuntime.ethers.getSigners();
+        console.log("Using the account:", caller.address);
+
+        const guard = await hardhatRuntime.ethers.getContractAt(
+            "BlacklistGuard",
+            taskArgs.guard
+        );
+
+        const delayABI = CONTRACT_ABIS['delay'].slice();
+        delayABI.push("function execTransactionFromModule(address,uint256,bytes,uint8) public returns (bool success)")
+
+        const delayModifier = new Contract(taskArgs.delay, delayABI, hardhatRuntime.ethers.provider);
+
+        const safeDeployment = getSafeSingletonDeployment({ version: "1.3.0" });
+        assert(safeDeployment !== undefined);
+        const avatar = new Contract(taskArgs.avatar, safeDeployment.abi, hardhatRuntime.ethers.provider);
+
+        const [user1] = await hardhatRuntime.ethers.getSigners();
+
+        let funcHash = "0x00000000";
+        if (taskArgs.funcsig !== null) {
+            funcHash = keccak256(utils.toUtf8Bytes(taskArgs.funcsig)).slice(0, 10);
+        }
+
+        const data = await guard.interface.encodeFunctionData("setTarget",
+            [taskArgs.target,
+            taskArgs.blockall,
+            taskArgs.blockdelegate,
+                funcHash,
+            taskArgs.blockfunc])
+
+        const delayData = delayModifier.interface.encodeFunctionData("execTransactionFromModule(address,uint256,bytes,uint8)", [guard.address, 0, data, 0]);
+
+        await sendSafeTransaction(user1, avatar, delayModifier.address, delayData, hardhatRuntime);
+
+    });
+
+
+task("execDelayedSetTarget", "Block a target address.")
+    .addParam("delay", "Address of the delay", undefined, types.string)
+    .addParam(
+        "guard",
+        "The address of the guard that you are seting up.",
+        undefined,
+        types.string
+    )
+    .addParam(
+        "target",
+        "The target address to be blocked.",
+        undefined,
+        types.string
+    )
+    .addParam(
+        "blockall",
+        "Block all access to the Target",
+        false,
+        types.boolean
+    )
+    .addParam(
+        "blockdelegate",
+        "Block delegatedCall",
+        false,
+        types.boolean
+    )
+    .addParam(
+        "funcsig",
+        "The signature of the target blocked function",
+        null,
+        types.string
+    )
+    .addParam(
+        "blockfunc",
+        "Block the target function",
+        false,
+        types.boolean
+    )
+    .setAction(async (taskArgs, hardhatRuntime) => {
+        const [caller] = await hardhatRuntime.ethers.getSigners();
+        console.log("Using the account:", caller.address);
+
+        const guard = await hardhatRuntime.ethers.getContractAt(
+            "BlacklistGuard",
+            taskArgs.guard
+        );
+
+        const delayABI = CONTRACT_ABIS['delay'].slice();
+        delayABI.push("function execTransactionFromModule(address,uint256,bytes,uint8) public returns (bool success)")
+        delayABI.push("function executeNextTx(address,uint256,bytes,uint8) public")
+
+        const delayModifier = new Contract(taskArgs.delay, delayABI, hardhatRuntime.ethers.provider);
+
+
+        const [user1] = await hardhatRuntime.ethers.getSigners();
+
+        let funcHash = "0x00000000";
+        if (taskArgs.funcsig !== null) {
+            funcHash = keccak256(utils.toUtf8Bytes(taskArgs.funcsig)).slice(0, 10);
+        }
+
+        const data = await guard.interface.encodeFunctionData("setTarget",
+            [taskArgs.target,
+            taskArgs.blockall,
+            taskArgs.blockdelegate,
+                funcHash,
+            taskArgs.blockfunc])
+
+        await delayModifier.connect(user1).executeNextTx(guard.address, 0, data, 0);
+
+    });
+
+
+
+
+const sendSafeTransaction = async (user1: Signer, avatar: Contract, to: string, data: string, hardhatRuntime: HardhatRuntimeEnvironment) => {
+    const nonce = await avatar.nonce()
+    const addressZero = hardhatRuntime.ethers.constants.AddressZero
+
+    const transHash = await avatar.connect(user1).getTransactionHash(to, 0, data, 0, 0, 0, 0, addressZero, addressZero, nonce);
+    const typedDataHash = utils.arrayify(transHash)
+    const signature = (await user1.signMessage(typedDataHash)).replace(/1b$/, "1f").replace(/1c$/, "20");
+    await avatar.connect(user1).execTransaction(to, 0, data, 0, 0, 0, 0, addressZero, addressZero, signature);
+}
+
 
 export { };
